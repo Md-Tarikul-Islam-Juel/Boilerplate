4bb71fe0a30817d6817e44f9aa7f23f84801d3b4
Md.Tarikul Islam Juel <md.tarikulislamjuel@gmail.com>
2022-08-08 12:08:05 +0600

    react update

--- React/src/index.js
+++ React/src/index.js
@@ -1,4 +1,3 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
@@ -6,5 +5,7 @@ import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App/>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
